b856edcf7c7751d061160e2601c538a0ac57cbc6
XRichardroidX <oguzieibehrichard@gmail.com>
2022-11-01 18:14:23 +0100

    Add webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+
+console.log( 'Hi' );
+
+const Name = document.querySelector( '.name' );
+
+Name.innerHTML = "Ok Ok!";
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require( 'path' );
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+    plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Output Management',
+        template: './src/template.html'
+    }),
+    ],
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve( __dirname, 'dist' ),
+    },
+    module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+    },
+    devServer: {
+    static: './dist',
+  },
+};
